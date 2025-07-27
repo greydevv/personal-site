a466faa20071d1bbf8f6fe6ffdcfcc3c710992c8
Greyson Murray <gr.murray@draftkings.com>
2025-07-27 11:07:06 -0400

    Remove dead code

--- src/resources/demarkate/editor.js
+++ src/resources/demarkate/editor.js
@@ -7,25 +7,15 @@ const output_window = document.getElementById("output_window");
 const translucent_dismisser = document.getElementById("translucent_dismisser")
 const raw_output = document.getElementById("output");
 
-// Performance tracking
-let n_keystrokes = 0;
-let render_time_sum = 0;
-let render_time_avg = 0;
+let duration = 0;
 
 function updateDebugDisplay(memoryUsage) {
   const avg_speed_el = document.getElementById("avg-speed-display");
   const mem_usage_el = document.getElementById("mem-usage-display");
 
   const mem_usage = module.allocator.mem.buffer.byteLength;
-  mem_usage_el.textContent = `${mem_usage} bytes`
-  avg_speed_el.textContent = `${render_time_avg.toFixed(5)}ms`;
-}
-
-function trackRenderTime(textLength, duration) {
-  n_keystrokes++;
-  render_time_sum += duration;
-  render_time_avg = render_time_sum / n_keystrokes;
-  updateDebugDisplay();
+  mem_usage_el.textContent = `${mem_usage} bytes allocated`
+  avg_speed_el.textContent = `${duration}ms`;
 }
 
 window.viewSourceClicked = function() {
@@ -62,7 +52,8 @@ function render() {
   const output = module.render(editor.value);
   const end_time = performance.now();
 
-  trackRenderTime(editor.value.length, end_time - start_time);
+  duration = (end_time - start_time).toFixed(5);
+  updateDebugDisplay();
 
   if (output.err_code === 0) {
     setPreview(output.html);
